d8f668e79b4775d1ca7fe29fdcc0959cee36dffb
Mehvish Sana <mehvishsana16@gmail.com>
2023-11-16 00:52:56 +0530

    max scale

--- app/head.js
+++ app/head.js
@@ -4,9 +4,8 @@ const Head = () => (
   <
     >
     <title> ahead
-    </title> <meta name="viewport"
-      content="width=device-width, initial-scale= 0.8 "
-    />
+    </title> <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=0.8">
+
     <link rel="icon"
       href="ahead_logo"
     />
