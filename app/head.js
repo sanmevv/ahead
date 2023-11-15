9bceead15b33b86e44f6153f2e660ac4052e3c1a
Mehvish Sana <mehvishsana16@gmail.com>
2023-11-16 00:34:42 +0530

    viewport fix

--- app/head.js
+++ app/head.js
@@ -5,7 +5,7 @@ const Head = () => (
     >
     <title> ahead
     </title> <meta name="viewport"
-      content="width=device-width, initial-scale=1"
+      content="width=device-width, initial-scale= 0.8 "
     />
     <link rel="icon"
       href="ahead_logo"
